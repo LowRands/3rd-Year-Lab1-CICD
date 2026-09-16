45841fe2e3fd026345f2e171bb76df82aa999279
EthanLourens-STUDENT <G00467054@atu.ie>
2026-09-16 14:53:50 +0100

    Start of Project

---
+++ src/main/java/Third_year_lab1_1/Application.java
@@ -0,0 +1,13 @@
+package Third_year_lab1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/Third_year_lab1_1/Model/Product.java
@@ -0,0 +1,4 @@
+package Third_year_lab1_1.Model;
+
+public class Product {
+}

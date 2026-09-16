218fea533ccaec372293d6cb8cd4148badd2ac69
EthanLourens-STUDENT <G00467054@atu.ie>
2026-09-16 14:56:43 +0100

    Added Product to model package

--- src/main/java/Third_year_lab1_1/Model/Product.java
+++ src/main/java/Third_year_lab1_1/Model/Product.java
@@ -1,4 +1,17 @@
 package Third_year_lab1_1.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
 }
+

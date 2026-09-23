44b47f1598a3f5a8dc5fae73423f4b63aeff62bf
EthanLourens-STUDENT <g00467054@atu.ie>
2026-09-23 14:05:54 +0100

    changed product to entity with auto generated id

--- src/main/java/Third_year_lab1_1/Model/Product.java
+++ src/main/java/Third_year_lab1_1/Model/Product.java
@@ -1,5 +1,9 @@
 package Third_year_lab1_1.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
